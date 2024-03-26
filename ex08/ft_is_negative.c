abeedcac4fa89f8c8c919d069375803a1af41e78
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-03-26 21:10:14 +0200

    void ft_is_negative(int n)

---
+++ ex08/ft_is_negative.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_negative.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/26 20:12:44 by irychkov          #+#    #+#             */
+/*   Updated: 2024/03/26 20:12:44 by irychkov         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+/*void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+*/
+void	ft_is_negative(int n)
+{
+	if (n < 0)
+		ft_putchar('N');
+	else
+		ft_putchar('P');
+}
+/*
+int	main(void)
+{
+	ft_is_negative(-5);
+	ft_is_negative(0);
+	ft_is_negative(5);
+	return 0;
+}*/
