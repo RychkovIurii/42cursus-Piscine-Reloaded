5c271519f7d018b63c8571f1a2930e3b99ad898b
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-03-26 21:17:33 +0200

    void ft_print_numbers(void)

--- ex07/ft_print_numbers.c
+++ ex07/ft_print_numbers.c
@@ -11,12 +11,13 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-
-/*void	ft_putchar(char c)
+/*
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
 */
+
 void	ft_print_numbers(void)
 {
 	char	c;
