56f6d4fa1a9dfdbe8995389153fa196a2c6489af
Rychkov Iurii <rychkoviurii@hotmail.com>
2024-04-06 17:34:38 +0300

    int	*ft_range(int min, int max)

--- ft_range.c
+++ ft_range.c
@@ -6,10 +6,11 @@
 /*   By: irychkov <irychkov@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/06 17:06:15 by irychkov          #+#    #+#             */
-/*   Updated: 2024/04/06 17:22:22 by irychkov         ###   ########.fr       */
+/*   Updated: 2024/04/06 17:34:06 by irychkov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
 
 int	*ft_range(int min, int max)
 {
@@ -19,11 +20,11 @@ int	*ft_range(int min, int max)
 
 	i = 0;
 	len = max - min;
-	if (min > max)
+	if (min >= max)
 	{
 		return NULL;
 	}
-	ptr = (int *)malloc(sizeof(int) * len);
+	ptr = (int *)malloc(len * sizeof(int));
 	if (ptr == NULL)
 		return NULL;
 	while (i < len)
@@ -31,5 +32,22 @@ int	*ft_range(int min, int max)
 		ptr[i] = min;
 		min++;
 	}
-	return (*ptr);
+	return (ptr);
 }
+/* 
+#include <stdio.h>
+int	main(void)
+{
+	int	*ptr;
+	int	i;
+
+	i = 0;
+	ptr = ft_range(5, 9);
+	printf("The elements of the array are:\n");
+	while (i < 4)
+	{
+		printf("%d\n", ptr[i]);
+		i++;
+	}
+	free(ptr);
+} */
